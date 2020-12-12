cda10561719261d4cb9a55798d5952011cfed4a4
unknown <anhvipkaka9999@gmail.com>
2020-12-12 14:29:31 +0700

    first commit

--- src/components/PopupSuccess.tsx
+++ src/components/PopupSuccess.tsx
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class PopupSuccess extends Component {
-    render() {
-        return (
-            <div className='poppup-success'>
-                Thêm vào giỏ hàng thành công
-            </div>
-        );
-    }
+  render() {
+    return <div className='poppup-success'>Thêm vào giỏ hàng thành công</div>;
+  }
 }
 
 export default PopupSuccess;
